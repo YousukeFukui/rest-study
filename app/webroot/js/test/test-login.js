d2fc3040e995f08c3c12c818e73e6cb5b2a20579
YousukeFukui <root@ip-172-31-14-89.ap-northeast-1.compute.internal>
2016-01-14 09:58:05 +0000

    vol/11-1

---
+++ app/webroot/js/test/test-login.js
@@ -0,0 +1,26 @@
+
+define(function (require) {
+	var UserModel = require('models/user-model');
+
+	var userModel = new UserModel();
+	return function login(done){
+		if (!userModel.isLoggedIn()) {
+			userModel.login(
+				'fukui',   //DB登録データに合わせる
+				'fukui',		//DB登録データに合わせる
+				function success(message) {
+					console.info("logged in!")
+					done();
+				},
+				function error(message) {
+					done('login error! ' + '\n' + message);
+				}
+			)
+		} else {
+			//ログイン済みなら終了
+			console.info('ログイン済み');
+			done();
+		}
+	}
+});
+
---
+++ app/webroot/js/test/tests.js
@@ -0,0 +1,6 @@
+define(function (require) {
+    //読み込み
+        var testTodoItemView = require('js/test/test-todo-item-view.js');
+     //実行
+        testTodoItemView();
+  });
